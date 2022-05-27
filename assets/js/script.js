9027fe7b37db78de559cced43f6ff2a78e232b02
Anillag <101154249+anillag@users.noreply.github.com>
2022-05-27 17:32:42 -0400

    Updated current conditions output in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,11 @@ const apiKey = "44c256ba557f09fec96378b158180589";
 const searchButtonEl = document.querySelector("#search-button");
 const searchFieldEl = document.querySelector("#search-field");
 const contentDivEl = document.querySelector("#content-div");
-
+const cityDateEl = document.querySelector("#city-date");
+const tempEl = document.querySelector("#temp");
+const windEl = document.querySelector("#wind");
+const humidityEl = document.querySelector("#humidity");
+const uvIndexEl = document.querySelector("#uv-index");
 
 function search(city) {
     event.preventDefault();
@@ -54,10 +58,10 @@ var fetchCityData = function(city) {
                         fetchCurrentConditions(city, lat, lon);
                     });
             } else {
-                alert("fetch.then, else response.err:  Error reponse " + response.statusText);
-            }
-        })
-}
+                alert("fetchCityData, fetch.then, else response.err:  Error response " + response.statusText);
+            };
+        });
+};
 
 var fetchCurrentConditions = function(city, lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
@@ -65,9 +69,40 @@ var fetchCurrentConditions = function(city, lat, lon) {
     fetch(apiUrl)
         .then(function(response) {
         if (response.ok) {
-
-        }
-        })
-}
+            console.log("fetchCurrentConditions, fetch.then:  Response is " + response + ".");
+            response.json()
+                .then(function(data) {
+                    console.log("fetchCurrentConditions, fetch.then, if response.ok, response.then:  data variable is " + JSON.stringify(data));
+                    var date = new Date(JSON.stringify(data.current.dt)*1000).toLocaleDateString("en-US");
+                    console.log("fetchCurrentConditions, fetch.then, if response.ok, response.then:  Current date is " + date);
+                    cityDateEl.textContent = city + " " + date;
+                    
+                    var temp = parseFloat((JSON.stringify(data.current.temp)-273.15)*1.8+32).toFixed(0);
+                    tempEl.textContent = "Temp: " + temp + "°F";
+                    var wind = parseFloat(JSON.stringify(data.current.wind_speed)).toFixed(0);
+                    windEl.textContent = "Wind: " + wind + " mph";
+                    var humidity = JSON.stringify(data.current.humidity);
+                    humidityEl.textContent = "Humidity: " + humidity + "%";
+                    var uvIndex = JSON.stringify(data.current.uvi);
+                    uvIndexEl.textContent = "UV Index: " + uvIndex;
+                    if (uvIndex <= 2.49) {
+                        uvIndexEl.classList.add("uv-2");
+                        uvIndexEl.classList.remove("uv-5", "uv-7", "uv-10");
+                    } else if (uvIndex >= 2.50 && uvIndex <= 5.49) {
+                        uvIndexEl.classList.add("uv-5");
+                        uvIndexEl.classList.remove("uv-2", "uv-7", "uv-10");
+                    } else if (uvIndex >= 5.50 && uvIndex <= 7.49) {
+                        uvIndexEl.classList.add("uv-7");
+                        uvIndexEl.classList.remove("uv-2", "uv-5", "uv-10");
+                    } else if (uvIndex >= 7.50 && uvIndex <= 10.49) {
+                        uvIndexEl.classList.add("uv-10");
+                        uvIndexEl.classList.remove("uv-2", "uv-5", "uv-7");
+                    }
+                });
+            } else {
+                alert("fetch.then, else response.err:  Error response " + response.statusText);
+            };
+        });
+};
 
 searchButtonEl.addEventListener("click", search);
